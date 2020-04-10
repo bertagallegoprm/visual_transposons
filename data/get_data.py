7808ef2647aa72ad1f4eda49e28e760cd39da26a
Berta Gallego <b.gallego@kew.org>
2020-04-10 19:46:30 +0100

    download all data in files

--- data/get_data.py
+++ data/get_data.py
@@ -8,11 +8,26 @@ def get_sequences():
     url = requests.get("https://www.arabidopsis.org/download_files/Genes/TAIR10_genome_release/TAIR10_transposable_elements/TAIR10_TE.fas")
     return url.text
 
+def get_names():
+    """
+    Get the TE names and attributes from the TAIR website
+    """
+    url = requests.get("https://www.arabidopsis.org/download_files/Genes/TAIR10_genome_release/TAIR10_transposable_elements/TAIR10_Transposable_Elements.txt")
+    return url.text
+
 
 def save_to_file(text):
     file = open(f"{text}.txt", "w")
     file.writelines(text) 
     file.close()
+
+  
+def get_all_te_data():
+    tair10_te_seq = get_sequences()
+    tair10_te_attributes = get_names()
+    save_to_file(tair10_te_seq)
+    save_to_file(tair10_te_attributes)
+
 
 
 

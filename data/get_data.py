e1b9b12f34dbadf7d3cd75c407fe698860b6522e
Berta Gallego <b.gallego@kew.org>
2020-04-10 20:24:19 +0100

    refactor get_data

--- data/get_data.py
+++ data/get_data.py
@@ -1,34 +1,38 @@
 
 import requests
+import datetime
 
-def get_sequences():
+
+def download_sequences_file():
     """
     Get the TE sequences from the TAIR website
+    and store it in text file.
     """
-    url = requests.get("https://www.arabidopsis.org/download_files/Genes/TAIR10_genome_release/TAIR10_transposable_elements/TAIR10_TE.fas")
-    return url.text
-
-def get_names():
-    """
-    Get the TE names and attributes from the TAIR website
-    """
-    url = requests.get("https://www.arabidopsis.org/download_files/Genes/TAIR10_genome_release/TAIR10_transposable_elements/TAIR10_Transposable_Elements.txt")
-    return url.text
-
-
-def save_to_file(text):
-    file = open(f"{text}.txt", "w")
-    file.writelines(text) 
+    url = requests.get("https://www.arabidopsis.org/download_files/Genes/TAIR10_genome_release/TAIR10_transposable_elements/TAIR10_TE.fas")  
+    file = open("te_sequences.txt", "w")
+    file.writelines(url.text) 
     file.close()
 
-  
-def get_all_te_data():
-    tair10_te_seq = get_sequences()
-    tair10_te_attributes = get_names()
-    save_to_file(tair10_te_seq)
-    save_to_file(tair10_te_attributes)
+
+def download_attributes_file():
+    """
+    Get the TE names and attributes from the TAIR website
+    and store it in text file.
+    """
+    url = requests.get("https://www.arabidopsis.org/download_files/Genes/TAIR10_genome_release/TAIR10_transposable_elements/TAIR10_Transposable_Elements.txt")
+    file = open("te_attributes.txt", "w")
+    file.writelines(url.text) 
+    file.close()
 
 
+def print_download_date():
+    last_download = str(datetime.date.today())
+    file = open("last_download.txt", "w")
+    file.write(last_download) 
+    file.close()
 
 
 if __name__ == "__main__":
+    download_sequences_file()
+    download_attributes_file()
+    print_download_date()

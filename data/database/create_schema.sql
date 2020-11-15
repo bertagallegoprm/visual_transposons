8bd2ab02f4d92a49f5c02dd865ec4ab27174bfc5
Berta Gallego <bertagallego@gmail.com>
2020-11-15 13:54:52 +0000

    Adjust schema to preserve data integrity
    Set FKs as not null and attributes as unique.

--- data/database/create_schema.sql
+++ data/database/create_schema.sql
@@ -6,23 +6,23 @@ USE transposons;
 
 CREATE TABLE IF NOT EXISTS chromosome(
     id INT NOT NULL AUTO_INCREMENT
-    , chromosomeNumber INT
+    , chromosomeNumber INT NOT NULL UNIQUE
     , PRIMARY KEY (id)
 );
 
 
 CREATE TABLE orientation(
     id INT NOT NULL AUTO_INCREMENT
-    ,isFivePrima VARCHAR(5)
+    ,isFivePrime VARCHAR(5) NOT NULL UNIQUE
     ,PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS location (
     id INT NOT NULL AUTO_INCREMENT
-    , minStart INT
+    , minStart INT 
     , maxEnd INT
-    , idOrientation INT
-    , idChromosome INT
+    , idOrientation INT NOT NULL
+    , idChromosome INT NOT NULL
     , PRIMARY KEY (id)
     , FOREIGN KEY fk_location_orientation (idOrientation) REFERENCES orientation (id)
     , FOREIGN KEY fk_location_chromosome (idChromosome) REFERENCES chromosome (id)
@@ -32,25 +32,23 @@ CREATE TABLE IF NOT EXISTS location (
 
 CREATE TABLE IF NOT EXISTS superFamily(
     id INT NOT NULL AUTO_INCREMENT
-    , superFamilyName VARCHAR(25)
+    , superFamilyName VARCHAR(25) NOT NULL UNIQUE
     , PRIMARY KEY (id)
 );
  
 CREATE TABLE IF NOT EXISTS family(
     id INT NOT NULL AUTO_INCREMENT
-    , familyName VARCHAR(25)
-    , idSuperFamily INT
+    , familyName VARCHAR(25) NOT NULL UNIQUE
+    , idSuperFamily INT NOT NULL
     , PRIMARY KEY (id)
     , FOREIGN KEY fk_family_superfamily (idSuperFamily) REFERENCES superFamily (id)
 ); 
 
 CREATE TABLE IF NOT EXISTS transposon(
     id INT NOT NULL AUTO_INCREMENT
-    , transposonName VARCHAR(10)
-    , idOrientation INT 
-    , idLocation INT 
-    , idFamily INT 
-    , idChromosome INT
+    , transposonName VARCHAR(10) NOT NULL UNIQUE
+    , idLocation INT NOT NULL
+    , idFamily INT NOT NULL
     , PRIMARY KEY (id)
     , FOREIGN KEY fk_transposon_location (idLocation) REFERENCES location (id)
     , FOREIGN KEY fk_transposon_family (idFamily) REFERENCES family (id)
